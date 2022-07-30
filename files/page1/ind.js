e16c217eb62fc2430608cd5c459cacc8805bf9b3
Sumit kumar  Maurya <62202835+sumitkumar2502@users.noreply.github.com>
2022-07-31 02:20:22 +0530

    Update ind.js

--- files/page1/ind.js
+++ files/page1/ind.js
@@ -1,22 +1,22 @@
 function changbg(a){
 
     if (a==1){
-        document.getElementById("sec2img").src="../../source/railway.jpg";
+        document.getElementById("sec2img").src="source/railway.jpg";
     }
     else if (a==2){
-        document.getElementById("sec2img").src="../../source/nuclear_plant.jpg";
+        document.getElementById("sec2img").src="source/nuclear_plant.jpg";
     }
     else if (a==3){
-        document.getElementById("sec2img").src="../../source/airport.jpg";
+        document.getElementById("sec2img").src="source/airport.jpg";
     }
     else if (a==4){
-        document.getElementById("sec2img").src="../../source/port.jpg";
+        document.getElementById("sec2img").src="source/port.jpg";
     }
     else if (a==5){
-        document.getElementById("sec2img").src="../../source/highway.jpg";
+        document.getElementById("sec2img").src="source/highway.jpg";
     }
     else if (a==6){
-        document.getElementById("sec2img").src="../../source/mining.jpg";
+        document.getElementById("sec2img").src="source/mining.jpg";
     }
    
 }
